1d9c6e17efa0efd98fae7120e8a0bef4900bccda
Ruy Valle <ricardo.valle@unryo.com>
2023-05-07 12:50:31 -0400

    Try to rewrite GAT from scratch again

--- code/torch_gat.py
+++ code/torch_gat.py
@@ -5,8 +5,8 @@ import scipy.sparse
 class GATHead(torch.nn.Module):
     def __init__(self, in_dim, out_dim, bias, in_drop=0.0, coef_drop=0.0, residual=False):
         super().__init__()
-        self.bias1 = bias
-        self.bias2 = torch.zeros(out_dim)
+        self.bias1 = torch.nn.Parameter(bias)
+        self.bias2 = torch.nn.Parameter(torch.zeros(out_dim))
         self.in_drop = in_drop
         self.coef_drop = coef_drop
         self.residual = residual
@@ -84,3 +84,46 @@ def planetoid_adj_to_petarv_adj(adj):
     for row in adj:
         petarv_adj[row[0], row[1]] = 1
     return petarv_adj
+
+class GATLayer(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, n_heads, dropout, concat=True):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.n_heads = n_heads
+        self.lin = torch.nn.Linear(in_dim, n_heads*out_dim)
+        self.src_proj = torch.nn.Parameter(torch.nn.init.xavier_uniform_(torch.Tensor(1, n_heads, out_dim)))
+        self.trg_proj = torch.nn.Parameter(torch.nn.init.xavier_uniform_(torch.Tensor(1, n_heads, out_dim)))
+        self.dropout = torch.nn.Dropout(dropout)
+        self.leakyRelu = torch.nn.LeakyReLU()
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.concat = concat
+
+    def forward(self, data, connectivity):
+        data1 = self.lin(self.dropout(data)).view(-1, self.n_heads, self.out_dim)
+        data2 = self.dropout(data1)
+        #data = torch.nn.functional.dropout(self.lin(torch.nn.functional.dropout(data, self.dropout)), self.dropout).view(-1, self.n_heads, self.out_dim)
+        src_scores = (data2 * self.src_proj).sum(dim=-1, keepdim=True).permute(1, 0, 2)
+        trg_scores = (data2 * self.trg_proj).sum(dim=-1, keepdim=True).permute(1, 2, 0)
+        scores = self.softmax(self.leakyRelu(src_scores + trg_scores) + connectivity)
+        data3 = torch.bmm(scores.float(), data2.permute(1, 0, 2).float()).permute(1, 0, 2)
+        # skip connection??
+
+        if self.concat:
+            data4 = data3.reshape(-1, self.n_heads*self.out_dim)
+        else:
+            data4 = data3.mean(dim=1)
+        # bias?
+        return data4
+
+class GATCora(torch.nn.Module):
+    def __init__(self, n_features, n_classes):
+        super().__init__()
+        self.layer1 = GATLayer(n_features, 8, 8, .6, True)
+        self.elu = torch.nn.ELU()
+        self.layer2 = GATLayer(64, n_classes, 1, .6, False)
+        self.softmax = torch.nn.Softmax(dim=-1)
+
+    def forward(self, x, connectivity):
+        x = self.elu(self.layer1(x, connectivity))
+        return self.softmax(self.layer2(x, connectivity))
